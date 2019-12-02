52c8078c2404600f38d9af82dcf4547a452de967
Aimee Barciauskas <aimee@developmentseed.org>
2019-12-02 11:54:56 -0500

    Modify jupyter run command

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 mount.davfs https://podaac-tools.jpl.nasa.gov/drive/files /mnt/podaac_drive
 source activate netcdf_to_zarr
-/opt/conda/bin/jupyter notebook --ip='*' --port=8888 --no-browser --allow-root
+jupyter notebook --ip='*' --port=8888 --no-browser --allow-root
 
