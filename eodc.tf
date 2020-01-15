b034ad8ee8736abff36768a5e286672ddaa035cc
Aimee Barciauskas <aimee@developmentseed.org>
2020-01-15 16:13:05 -0500

    Updated notebook

--- eodc.tf
+++ eodc.tf
@@ -62,19 +62,10 @@ data "template_file" "netcdf_to_zarr_task_definition" {
 resource "aws_launch_configuration" "as_conf" {
   name          = "eodc-ecs-cluster"
   image_id      = data.aws_ami.amazon-linux-2-ecs-optimized.id
-  instance_type = "m5.8xlarge"
+  instance_type = "m5.16xlarge"
   user_data = data.template_file.ecs_instance_init.rendered
   key_name = var.keypair
   iam_instance_profile = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:instance-profile/ecsInstanceRole"
-
-  ebs_block_device {
-    device_name = "/dev/xvdcz"
-    volume_size = 200
-  }
-
-  root_block_device {
-    volume_size = 200
-  }
 }
 
 resource "aws_autoscaling_group" "ecs_asg" {
